67302cd0cc4ba657111501b827203f5af95bf7bf
Daniel de Carvalho <danielcrv92@gmail.com>
2017-06-16 02:10:07 -0300

    Inicio do Projeto

---
+++ js-es6/app.js
@@ -0,0 +1,26 @@
+class Painel{
+
+    constructor(tipo, nome){
+        let this.nome = nome;
+        let this.tipo = tipo;
+    }
+
+    get nome(){
+        return this.nome;
+    }
+
+    set nome(nome){
+        this.nome = nome;
+    }
+
+    get tipo(){
+        return this.tipo;
+    }
+
+    set tipo(tipo){
+        this.tipo = tipo;
+    }
+
+
+
+}
